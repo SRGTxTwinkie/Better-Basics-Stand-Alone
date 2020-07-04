49ffa10f074c11ba5d2cce0dd2bd17352dc748f7
SRGTxTwinkie <54576833+SRGTxTwinkie@users.noreply.github.com>
2020-07-04 18:59:44 -0400

    Its a library

---
+++ Arrays.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterBasicsLibrary
+{
+    public class Arrays
+    {
+
+        // Pass a double and an array into this to return a new array with the old and new data in it
+
+        public static double[] Add(double to_add, double[] to_copy)
+        {
+            double[] final = new double[to_copy.Length + 1];
+
+            for (int i = 0; i < to_copy.Length; i++)
+            {
+                final[i] = to_copy[i];
+            }
+
+            final[final.Length - 1] = to_add;
+
+            return final;
+        }
+
+
+        // Pass an int and an array into this to return a new array with the old and new data in it
+
+        public static int[] Add(int to_add, int[] to_copy)
+        {
+            int[] final = new int[to_copy.Length + 1];
+
+            for (int i = 0; i < to_copy.Length; i++)
+            {
+                final[i] = to_copy[i];
+            }
+
+            final[final.Length - 1] = to_add;
+
+            return final;
+        }
+
+        // Pass a string and an array into this to return a new array with the old and new data in it
+
+        public static string[] Add(string to_add, string[] to_copy)
+        {
+            string[] final = new string[to_copy.Length + 1];
+
+            for (int i = 0; i < to_copy.Length; i++)
+            {
+                final[i] = to_copy[i];
+            }
+
+            final[final.Length - 1] = to_add;
+
+            return final;
+        }
+
+        // Pass an int and an array into this to check if there is a matching int
+
+        public static bool InArray(int[] check_array, int to_check)
+        {
+            bool in_array = false;
+
+            for (int i = 0; i < check_array.Length; i++)
+            {
+                if (check_array[i] == to_check)
+                {
+                    in_array = true;
+                }
+
+            }
+
+            return in_array;
+        }
+
+        // Pass an string and an array into this to check if there is a matching string
+
+        public static bool InArray(string[] check_array, string to_check)
+        {
+            bool in_array = false;
+
+            for (int i = 0; i < check_array.Length; i++)
+            {
+                if (check_array[i].Equals(to_check))
+                {
+                    in_array = true;
+                }
+
+            }
+
+            return in_array;
+        }
+
+        //Pass a double and an array into this to check if there is a matching double
+
+        public static bool InArray(double[] check_array, double to_check)
+        {
+            bool in_array = false;
+
+            for (int i = 0; i < check_array.Length; i++)
+            {
+                if (check_array[i] == to_check)
+                {
+                    in_array = true;
+                }
+
+            }
+
+            return in_array;
+        }
+
+    }
+}
---
+++ StringParse.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterBasicsLibrary
+{
+    public class StringParse
+    {
+
+        // Call this to Parse a string from a passed int32
+        //
+        // I.E.
+        // string equals_one = "1"; 
+        // Console.WriteLine(StringParse.Int(equals_one).GetType())
+        // 
+        // Will produce...
+        // System.Int32
+
+        public static int IntVarParse(string string_in) { 
+       
+            int final_int;
+            bool is_int = int.TryParse(string_in, out final_int);
+
+            if(is_int)
+            {
+                return final_int;
+            }
+            else
+            {
+                return 0;
+            }
+            
+        }
+
+        // Call this to read and parse an int from the console
+        //
+        // I.E.
+        // Console.Write("What number would you like to pick: ");
+        // var string_from_int = StringParse.Int();
+
+        public static int Int()
+        {
+            string string_in;
+            int final_int;
+            bool is_int;
+
+            string_in = Console.ReadLine();
+
+            is_int = int.TryParse(string_in, out final_int);
+
+            while (!is_int)
+            {
+                string_in = Console.ReadLine();
+                is_int = int.TryParse(string_in, out final_int);
+            }
+
+            return final_int;
+        }
+
+        // Call this to print out a prompt, read, and parse an int from the console
+        //
+        // I.E.
+        // var string_from_int = StringParse.Int("What number would you like: ");
+
+        public static int Int(string output)
+        {
+            Console.Write("{0}: ", output);
+            string string_in = Console.ReadLine();
+            int final_int;
+            bool is_int;
+
+            is_int = int.TryParse(string_in, out final_int);
+
+            while (!is_int)
+            {
+                string_in = Console.ReadLine();
+                is_int = int.TryParse(string_in, out final_int);
+            }
+
+            return final_int;
+        }
+
+        //Same as int
+
+        public static double Double()
+        {
+            string string_in = Console.ReadLine();
+            double final_double;
+            bool is_double;
+
+            is_double = double.TryParse(string_in, out final_double);
+
+            while (!is_double)
+            {
+                string_in = Console.ReadLine();
+                is_double = double.TryParse(string_in, out final_double);
+            }
+
+            return final_double;
+        }
+
+        // Same as int
+
+        public static double Double(string output)
+        {
+            Console.Write(output + ": ");
+            string string_in = Console.ReadLine();
+            double final_double;
+            bool is_double;
+
+            is_double = double.TryParse(string_in, out final_double);
+
+            while (!is_double)
+            {
+                string_in = Console.ReadLine();
+                is_double = double.TryParse(string_in, out final_double);
+            }
+
+            return final_double;
+        }
+    }
+}
